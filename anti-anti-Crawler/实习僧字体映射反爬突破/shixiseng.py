96a1ed0a2c0fcdc7badd7658fda7481b7f6a8875
wwuu0009 <jqmhpj@gmail.com>
2020-06-07 02:06:20 +0800

    update dic

--- anti-anti-Crawler/实习僧字体映射反爬突破/shixiseng.py
+++ anti-anti-Crawler/实习僧字体映射反爬突破/shixiseng.py
@@ -1,18 +1,7 @@
 import requests
 from lxml import etree
-#from items import wordDict
-wordDict = {
-        '\ue4b1':'0',
-        '\ue8d8':'1',
-        '\ue770':'2',
-        '\ue714':'3',
-        '\ue4c3':'4',
-        '\ue054':'5',
-        '\uf7d5':'6',
-        '\uf583':'7',
-        '\ueef6':'8',
-        '\uf01b':'9'
-}
+from items import wordDict
+
 
 url = 'https://www.shixiseng.com/interns?keyword=%E7%88%AC%E8%99%AB&page=1&city=%E5%85%A8%E5%9B%BD'
 response = requests.get(url)
