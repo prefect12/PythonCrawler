4cfc9d1e5500c0392abe375ea6707fe956c28b5e
wwuu0009 <jqmhpj@gmail.com>
2020-06-08 01:09:40 +0800

    update readMe

--- Crawler/tb/taobao.py
+++ Crawler/tb/taobao.py
@@ -17,46 +17,60 @@ def get_next_page(browser):
         return False
 
 
-options = Options()
-url = 'https://www.taobao.com/'
-options.add_experimental_option('excludeSwitches', ['enable-automation'])
-browser = webdriver.Chrome('./chromedriver.exe',options=options)
-browser.get(url)
-items = browser.find_elements_by_xpath('//div[@class="items"]/div[@class="item J_MouserOnverReq  "]')
+def get_browser():
+    options = Options()
+    url = 'https://www.taobao.com/'
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    browser = webdriver.Chrome('./chromedriver.exe',options=options)
+    browser.get(url)
+    return browser
 
 
 
-key_word = input("请输入你要搜索的商品：")
+def login_search(browser):
 
-browser.find_element_by_id("q").send_keys(key_word)
-browser.find_element_by_class_name('btn-search').click()
-browser.maximize_window()
-
-time.sleep(15)
-page_info = browser.find_element_by_xpath('//div[@class="total"]').text
-page = re.findall("(\d+)",page_info)[0]
-numOfItem = 0
-
-
-while True:
-    items = browser.find_elements_by_xpath('//div[@class="items"]/div[@class="item J_MouserOnverReq  "]')
-    current_page = browser.find_element_by_xpath('//li[@class="item active"]/span').text
-    print('page:',current_page)
-    for item in items:
-
-        pro_desc = item.find_element_by_xpath('.//div[@class="row row-2 title"]/a').text
-        item_url = item.find_element_by_xpath('.//div[@class="row row-2 title"]/a').get_attribute('href')
-        pro_price = item.find_element_by_xpath('.//strong').text
-        buy_num = item.find_element_by_xpath('.//div[@class="deal-cnt"]').text
-        shop_url = item.find_element_by_xpath('.//div[@class="shop"]/a').get_attribute('href')
-        shop = item.find_element_by_xpath('.//div[@class="shop"]/a').text
-        address = item.find_element_by_xpath('.//div[@class="location"]').text
-        
-        with open('{}.csv'.format(key_word), mode='a', newline='', encoding='utf-8-sig') as f:
-            csv_writer = csv.writer(f, delimiter=',')
-            csv_writer.writerow([pro_desc,item_url,pro_price, buy_num, shop,shop_url, address])
-            numOfItem += 1
-    print('已经抓取了%s条数据'%(numOfItem))
+    key_word = input("请输入你要搜索的商品：")
     
-    if not get_next_page(browser):
-        break
+    browser.find_element_by_id("q").send_keys(key_word)
+    browser.find_element_by_class_name('btn-search').click()
+    browser.maximize_window()
+    
+    time.sleep(20)
+
+#    page_info = browser.find_element_by_xpath('//div[@class="total"]').text
+#    page = re.findall("(\d+)",page_info)[0]
+
+
+def downloader(browser):
+    
+    numOfItem = 0
+    while True:
+        items = browser.find_elements_by_xpath('//div[@class="items"]/div[@class="item J_MouserOnverReq  "]')
+        current_page = browser.find_element_by_xpath('//li[@class="item active"]/span').text
+        print('page:',current_page)
+        for item in items:
+    
+            pro_desc = item.find_element_by_xpath('.//div[@class="row row-2 title"]/a').text
+            item_url = item.find_element_by_xpath('.//div[@class="row row-2 title"]/a').get_attribute('href')
+            pro_price = item.find_element_by_xpath('.//strong').text
+            buy_num = item.find_element_by_xpath('.//div[@class="deal-cnt"]').text
+            shop_url = item.find_element_by_xpath('.//div[@class="shop"]/a').get_attribute('href')
+            shop = item.find_element_by_xpath('.//div[@class="shop"]/a').text
+            address = item.find_element_by_xpath('.//div[@class="location"]').text
+            
+            with open('{}.csv'.format(key_word), mode='a', newline='', encoding='utf-8-sig') as f:
+                csv_writer = csv.writer(f, delimiter=',')
+                csv_writer.writerow([pro_desc,item_url,pro_price, buy_num, shop,shop_url, address])
+                numOfItem += 1
+        print('已经抓取了%s条数据'%(numOfItem))
+        
+        if not get_next_page(browser):
+            break
+        
+def main():
+    browser = get_browser()
+    login_search(browser)
+    downloader(browser)
+
+if __name__ == "__main__:
+    main()
